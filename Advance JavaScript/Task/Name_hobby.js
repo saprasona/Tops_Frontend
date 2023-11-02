01dd5d19e12bb429cf835e07ef5f29417a2ede96
sonas <sonasapra007@gmail.com>
2023-11-02 23:36:52 +0530

    changes Done

--- Advance JavaScript/Task/Name_hobby.js
+++ Advance JavaScript/Task/Name_hobby.js
@@ -2,35 +2,35 @@ let userData = [
   {
     name: "Vipul",
     age: 30,
-    hobby: ["Gaming", "Travel", "Playing"],
+    Hobbies: ["Gaming", "Travel", "Playing"],
   },
   {
     name: "Rahul",
     age: 25,
-    hobby: ["Drawing", "playing", "Reading"],
+    Hobbies: ["Drawing", "playing", "Reading"],
   },
   {
     name: "Sona",
     age: 34,
-    hobby: ["Reading", "Drawing", "Cooking"],
+    Hobbies: ["Reading", "Drawing", "Cooking"],
   },
   {
     name: "Alpesh",
     age: 30,
-    hobby: ["Playing", "Reading", "Writing"],
+    Hobbies: ["Playing", "Reading", "Writing"],
   },
   {
     name: "Jency",
     age: 9,
-    hobby: ["Drawing", "Travel", "coloring"],
+    Hobbies: ["Drawing", "Travel", "coloring"],
   },
   {
     name: "Veer",
     age: 8,
-    hobby: ["Gaming", "Eating", "playing"]
+    Hobbies: ["Gaming", "Eating", "playing"],
   },
 ];
 
-for (let i = 0; i <= 4; i++) {
-  console.log("My Name is", userData[i].name + " My Hobby is " + userData[i].hobby[1]);
+for (let i = 0; i <= 5; i++) {
+  console.log("My Name is", userData[i].name + " My Hobbies " + userData[i].Hobbies[0] + " , " + userData[i].Hobbies[1] + " , " + userData[i].Hobbies[2]);
 }
