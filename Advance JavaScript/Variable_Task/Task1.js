3659826a4acd52b947b7bef7feec42e7d4e53903
sonas <sonasapra007@gmail.com>
2023-12-04 22:33:07 +0530

    changes done

---
+++ Advance JavaScript/Variable_Task/Task1.js
@@ -0,0 +1,35 @@
+let a = 0;
+console.log("==>",a); //==>working
+
+//  let "a" = 0;
+// console.log("==>a",a);  //==>not working
+
+// let 'a' = 0;
+// console.log("==>",a);  //==>not working
+
+// let`10` = 0;
+// console.log("==>",a);  //==>not working
+
+// let 1 = 0;
+// console.log("==>",a);  //==>not working
+
+//==========================================================
+// let x = 100
+// var y = 100
+// const z = 100
+// const m
+{
+    //   x= 90
+    //   y = 90;
+    // z= 90
+    // console.log(x);  //==>90
+    //   console.log(y); //==>90
+    //   console.log(z);  //Assignment to constant variable
+  //   console.log(m);  // Missing initializer in const declaration
+  } 
+  
+  // console.log(x);  //==>90
+  // console.log(y);  //==>90
+  // console.log(z);  //Assignment to constant variable
+  
+  
---
+++ Advance JavaScript/Variable_Task/Task2.js
@@ -0,0 +1,14 @@
+// =================Task2==================
+  // console.log(x)  //x is not defined
+  // console.log(y)  //undefined
+  // console.log(z)  //z is not defined
+  
+  {
+    let x = 100
+    var y = 100
+    const z = 100
+}
+
+// console.log(x)  //x is not defined
+console.log(y)  //100
+// console.log(z)  //z is not defined
