7ce068f3afe6c9a67c43641b562f09b0e313d658
sonas <sonasapra007@gmail.com>
2024-05-16 00:28:55 +0530

    changes done

--- React_Js/React-Project/src/ui/Page/Admin/Product/ProductTable.jsx
+++ React_Js/React-Project/src/ui/Page/Admin/Product/ProductTable.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "flowbite-react";
-import { FaRegEdit } from "react-icons/fa";
+import { FaRegEdit, FaEye } from "react-icons/fa";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import ReactPaginate from "react-paginate";
-import { FaEye } from "react-icons/fa";
+
 export default function ProductTable({
   updateHandler,
   deleteHandler,
@@ -12,104 +12,116 @@ export default function ProductTable({
   setPagination,
   pagination,
   searchQuery,
-  viewHandler
+  viewHandler,
 }) {
   const sizeData = ["1.5", "2", "3", "4"];
-  const handlePageClick = (event) => {
-      setPagination({ ...pagination, page: event?.selected + 1 });
-    
+  const [itemsPerPage, setItemsPerPage] = useState(10); // State to manage items per page
+
+  // Event handler for changing items per page
+  const handleItemsPerPageChange = (event) => {
+    setItemsPerPage(parseInt(event.target.value));
+    setPagination({ ...pagination, limit: parseInt(event.target.value) }); // Update pagination limit
   };
+
+  const handlePageClick = (event) => {
+    setPagination({ ...pagination, page: event?.selected + 1 });
+  };
+
   return (
     <div>
       <div className="py-8">
         <Table responsive>
           <thead>
-            <tr className="text-2xl text-black bg-[#d6b8b9e2] ">
-              <th>Sr.No</th>
-              <th>Image</th>
-              <th>Brand </th>
-              <th>Gender</th>
-              <th>Price</th>
-              <th>category</th>
-              <th>Size</th>
-              <th>Action</th>
+            <tr className="text-xl text-[#832729] bg-[#d6b8b9e2]">
+              <th className="px-4 py-2">Sr.No</th>
+              <th className="px-4 py-2">Image</th>
+              <th className="px-4 py-2">Title</th>
+              <th className="px-4 py-2">Brand</th>
+              <th className="px-4 py-2">Gender</th>
+              <th className="px-4 py-2">Price</th>
+              <th className="px-4 py-2">Category</th>
+              <th className="px-4 py-2">Size</th>
+              <th className="px-4 py-2">Action</th>
             </tr>
           </thead>
-          <tbody className="text-lg ">
-            {productdata?.map?.((e, i) => {
-                return (
-                  <tr key={i} className="border-b-[2px] ">
-                    <td scope="row" className="px-[2rem] py-8">
-                      {i + 1}
-                    </td>
-                    <td>
-                      <img src={e?.thumbnail} className="w-[70px] h-[70px]" />
-                    </td>
-                    <td className="capitalize">{e?.brand}</td>
-                    <td className="capitalize">{e?.gender}</td>
-                    <td>{e?.price}</td>
-
-                    <td className="">
-                      <div className="grid  text-uppercase">
-                        {e?.category?.map?.((category, i) => (
-                          <th key={i} className="capitalize">
-                            {category}
-                          </th>
-                        ))}
-                      </div>
-                    </td>
-
-                    <td className="">
-                      <div className="flex gap-2">
-                        {sizeData?.map((size, i) => (
-                          <span
-                            key={i}
-                            className={
-                              e?.size?.includes(size)
-                                ? ` rounded-3xl h-[40px] w-[40px] flex justify-center items-center p-3 font-semibold text-black bg-gray-200`
-                                : " rounded-3xl h-[40px] w-[40px] flex justify-center items-center p-3 font-semibold text-gray-400 bg-gray-100"
-                            }
-                          >
-                            {size}
-                          </span>
-                        ))}
-                      </div>
-                    </td>
-
-                    <td>
-                      <div className="flex gap-3">
-                      <FaEye className="text-2xl cursor-pointer text-green-500"
-                      onClick={viewHandler}/>
-                        <FaRegEdit
-                          className="text-blue-700 text-2xl cursor-pointer "
-                          onClick={updateHandler}
-                        />
-                        <RiDeleteBin5Line
-                          className="text-danger text-2xl text-red-600 cursor-pointer"
-                          onClick={() => deleteHandler(e._id)}
-                        />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+          <tbody className="text-lg">
+            {productdata?.map((e, i) => (
+              <tr key={e._id} className="border-b-2">
+                <td className="px-4 py-2">{i + 1}</td>
+                <td className="px-4 py-2">
+                  <img src={e?.thumbnail} alt="Product" className="w-16 h-16 object-cover rounded" />
+                </td>
+                <td className="px-4 py-2 capitalize">{e?.title}</td>
+                <td className="px-4 py-2 capitalize">{e?.brand}</td>
+                <td className="px-4 py-2 capitalize">{e?.gender}</td>
+                <td className="px-4 py-2">{e?.price}</td>
+                <td className="px-4 py-2">
+                  <div className="px-4 py-2 capitalize">
+                    {e?.category?.map((category, index) => (
+                      <div key={index}>{category}</div>
+                    ))}
+                  </div>
+                </td>
+                <td className="px-4 py-2">
+                  <div className="flex gap-2">
+                    {sizeData.map((size, index) => (
+                      <span
+                        key={index}
+                        className={`rounded-full h-10 w-10 flex justify-center items-center font-semibold text-gray-400 ${e?.size?.includes(size) ? 'bg-gray-200 text-black' : 'bg-gray-100'}`}
+                      >
+                        {size}
+                      </span>
+                    ))}
+                  </div>
+                </td>
+                <td className="px-4 py-2">
+                  <div className="flex gap-3">
+                    <FaEye
+                      className="text-2xl cursor-pointer text-green-500"
+                      onClick={() => viewHandler(e._id)}
+                    />
+                    <FaRegEdit
+                      className="text-blue-700 text-2xl cursor-pointer"
+                      onClick={() => updateHandler(e._id)}
+                    />
+                    <RiDeleteBin5Line
+                      className="text-red-600 text-2xl cursor-pointer"
+                      onClick={() => deleteHandler(e._id)}
+                    />
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-3 items-center">
+          <span>Show:</span>
+          <select
+            className="p-2 border rounded-md"
+            value={itemsPerPage}
+            onChange={handleItemsPerPageChange}
+          >
+            <option value={10}>10</option>
+            <option value={25}>25</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </select>
+        </div>
         <ReactPaginate
-          className="flex items-center  gap-2 "
+          className="flex items-center gap-2"
           pageLinkClassName="border p-3"
-          nextLinkClassName="border bg-blue-500 text-white  p-3 text-xl"
-          previousLinkClassName="border bg-blue-500 text-white text-xl  p-3"
-          activeLinkClassName="bg-red-500 text-white  "
-          disabledLinkClassName="bg-blue-300"
+          nextLinkClassName="border bg-[#832729] text-white p-3 text-xl"
+          previousLinkClassName="border bg-[#832729] text-white text-xl p-3"
+          activeLinkClassName="bg-red-500 text-white"
+          disabledLinkClassName="bg-[#832729]"
           breakLabel="..."
           nextLabel=">"
           onPageChange={handlePageClick}
           pageRangeDisplayed={5}
-          pageCount={pagination.totalProduct / pagination?.limit}
-          previousLabel="< "
+          pageCount={Math.ceil(pagination.totalProduct / pagination?.limit)}
+          previousLabel="<"
           renderOnZeroPageCount={null}
         />
       </div>
